a8472823999951e4222de7e4a8b04f795f2cb795
mrpapercut <mischa.rodermond@gmail.com>
2015-07-15 14:04:29 +0200

    Testbutton for notifications

--- js/components/Container.js
+++ js/components/Container.js
@@ -2,13 +2,16 @@
 
 import React from "react";
 
+import contains from 'ramda/src/contains';
+
 import {GetContext, StateStream} from '../mixins';
+import notifications from '../lib/notifications';
 
 import Users from './Users';
 import Tasks from './Tasks';
 import Projects from './Projects';
 
-const {div} = React.DOM;
+const {div, button} = React.DOM;
 const cf = React.createFactory;
 
 const users = cf(Users);
@@ -22,7 +25,8 @@ const Container = React.createClass({
 	stateStream() {
 		return this.ctx().stores.appstore
 			.map(store => ({
-				currentPage: store.getCurrentPage()
+				currentPage: store.getCurrentPage(),
+				notifications: []
 			})
 		);
 	},
@@ -31,11 +35,30 @@ const Container = React.createClass({
 		console.log(this.state.currentPage);
 	},
 
+	notify() {
+		const notif = notifications.create({
+			title: 'Mischa sent you a message',
+			message: 'ooooh fuck yeah',
+			onClick: this.notifyClick
+		}, (id) => (this.state.notifications.push(id)));
+	},
+
+	notifyClick(id) {
+		console.log(id, this.state.notifications);
+		if (contains(id, this.state.notifications)) {
+			console.log('found the notification:', id);
+		}
+	},
+
 	render() {
 		const ctx = this.ctx();
 
 		return (
 			div({},
+				button({
+					ref: 'notify',
+					onClick: () => this.notify()
+				}, 'Notify!'),
 				users({ctx: ctx}),
 				tasks({ctx: ctx}),
 				projects({ctx: ctx})
